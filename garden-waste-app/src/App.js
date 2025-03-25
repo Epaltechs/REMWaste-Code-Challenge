8233d5fe7a95cb9841ad501f19faba31fac893d5
Epaltechs <emmapaul247@gmail.com>
2025-03-25 17:19:33 +0100

    update data

--- garden-waste-app/src/App.js
+++ garden-waste-app/src/App.js
@@ -1,8 +1,27 @@
-import Waste from "./Waste"
+import { getWastes } from "./data/waste"
+import { getImageURL } from "./utils/image-util";
 
 function App() {
-  return (
-          <Waste />
+  const wastes = getWastes();
+    return (
+      <ul className="wastes">
+      {
+        wastes.map(waste => (
+           <li
+            className="waste-card" 
+            key={waste.id}>
+            <img 
+              src={getImageURL(waste.postcode)} 
+              alt={waste.name} 
+              className="waste-card > img" />
+            <h2 className="info-group > img">{waste.size}</h2>
+            <p>{waste.hire_period_days}</p>
+            <h3 className="info-group > p">{waste.price_before_vat}</h3>
+            
+           </li> 
+        ))
+      }
+    </ul>
   )
 }
 
